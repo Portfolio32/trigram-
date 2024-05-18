ed387a7e733383903fab31d33e77495d86bf6b66
Joey Wan <joeyhongjiwan@gmail.com>
2024-05-18 17:49:23 -0400

    added comments

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import torch
 
-
+#Read all the names 
 words = open("names.txt").read().splitlines()
 
-characters = list(sorted(set(''.join(words))))
-characters.insert(0, '.')
-character_pairs = []
+#All the letters plus period at index 0
+characters = ['.'] + list(sorted(set(''.join(words))))
 
-for ch1 in characters:
-    for ch2 in characters:
-        character_pairs.append(ch1 + ch2)
+#list comprehension to get all pairs 
+character_pairs = [ch1 + ch2 for ch1 in characters for ch2 in characters]
 
 
+#Mapping of characters to numbers and vice versa
 stoi_pair = {s:i for i,s in enumerate(character_pairs)}
 itos_pair = {s:i for i,s in stoi_pair.items()}
 stoi = {s:i for i, s in enumerate(characters)}
 itos = {s:i for i,s in stoi.items()}
 
+
+#Storing the counts of each trigram
 N = torch.zeros((729,27), dtype=torch.int32)
 
 for w in words: 
@@ -25,7 +26,29 @@ for w in words:
         ch1ch2 = ch1 + ch2
         ix1 = stoi_pair[ch1ch2]
         ix2 = stoi[ch3]
-        N[ix1, ix2] += 1
+        N[ix1,ix2] += 1
+
+
+#model smoothing and probability matrix
+P = (N + 1).float()
+P = P / P.sum(1, keepdim=True)
+
+
+ll = 0.0
+n = 0
+
+for w in words: 
+    w = ['.'] + list(w) + ['.']
+    for ch1, ch2, ch3 in zip(w, w[1:], w[2:]):
+        ch1ch2 = ch1 + ch2
+        ix1 = stoi_pair[ch1ch2]
+        ix2 = stoi[ch3]
+        probs = P[ix1,ix2]
+        loglikelyhood = torch.log(probs)
+        ll =+ loglikelyhood
+        n += 1
+
+nll = -(ll / n)
 
 
 
